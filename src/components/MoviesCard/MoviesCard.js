ef72e5fca816f2cabadff1d4d75ea2373acaeecc
ivan1981sololov <94713393+ivan1981sololov@users.noreply.github.com>
2022-08-11 22:07:50 +0300

    initial

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,23 +1,105 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BASE_MOVIES_URL, MINUTES_IN_HOUR } from '../../utils/constants';
 import './MoviesCard.css';
-import cardImage from '../../images/photo.png';
 
-function MoviesCard({ isMovies }) {
+function MoviesCard(props) {
+
+  const {
+    cardId,
+    cardImage,
+    cardNameRu,
+    cardDuration,
+    cardTrailerLink,
+    handleLikeMovie,
+    handleMovieDelete,
+    isMovies,
+    savedMovies,
+  } = props;
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [savedMovieId, setSavedMovieId] = useState('');
+
+  const MOVIE_HOURS = (parseInt(cardDuration / MINUTES_IN_HOUR));
+  const MOVIE_MINUTES = cardDuration % MINUTES_IN_HOUR;
+
+  useEffect(() => {
+    isMovies && savedMovies.map((movie) => {
+      if (movie.movieId === cardId) {
+        setSavedMovieId(movie._id)
+      }
+    })
+  }, [savedMovies])
+
+  useEffect(() => {
+    const savedMoviesMovieId = isMovies && savedMovies.map((movie) => {
+      return movie.movieId
+    })
+    if (isMovies && savedMoviesMovieId.includes(cardId)) {
+      setIsLiked(true)
+    }
+  }, [savedMovies])
+
+  function likeMovie() {
+    handleLikeMovie(
+      {
+        duration: cardDuration,
+        url: cardImage,
+        trailerLink: cardTrailerLink,
+        nameRU: cardNameRu,
+        id: cardId,
+      }
+    )
+  }
+
+  function deleteMovie() {
+    handleMovieDelete(
+      {
+        _id: cardId,
+      }
+    )
+  }
+
+  function dislikeMovie() {
+    handleMovieDelete(
+      {
+        _id: savedMovieId,
+      }
+    )
+    setIsLiked(false)
+  }
+
   return (
     <div className='movies-card'>
       <div className='movies-card__main-container'>
         <div className='movies-card__info-container'>
-          <h4 className='movies-card__title'>33 слова о дизайне</h4>
-          <p className='movies-card__subtitle'>1ч42м</p>
+          <h4 className='movies-card__title'>{cardNameRu}</h4>
+          <p className='movies-card__subtitle'>
+            {MOVIE_HOURS > 0 && (`${MOVIE_HOURS}ч`)}{MOVIE_MINUTES > 0 && (`${MOVIE_MINUTES}м`)}
+          </p>
         </div>
         {isMovies && (
-          <button className='movies-card__like-button movies-card__like-button_active'></button>
+          <button
+            className={`movies-card__like-button ${isLiked && 'movies-card__like-button_active'}`}
+            onClick={isLiked ? dislikeMovie : likeMovie}></button>
         )}
         {!isMovies && (
-          <button className='movies-card__delete-button'></button>
+          <button className='movies-card__delete-button' onClick={deleteMovie}></button>
         )}
       </div>
-      <img className='movies-card__image' src={cardImage} alt='movie' />
+      <a href={cardTrailerLink} target='_blank' rel='noopener noreferrer'>
+        {isMovies && (
+          <img
+            className='movies-card__image'
+            src={`${BASE_MOVIES_URL}${cardImage}`}
+            alt={cardNameRu} />
+        )}
+        {!isMovies && (
+          <img
+            className='movies-card__image'
+            src={cardImage}
+            alt={cardNameRu} />
+        )}
+      </a>
     </div>
   )
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,105 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Register.css'
+import { REGEX_EMAIL, REGEX_NAME } from '../../utils/constants';
+import './Register.css';
 
-function Register() {
+function Register(props) {
+
+  const {
+    handleRegistration,
+    setErrorData,
+    errorData,
+  } = props;
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [nameMessage, setNameMessage] = useState('');
+
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [emailMessage, setEmailMessage] = useState('');
+
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [passwordMessage, setPasswordMessage] = useState('');
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    validateAll();
+    validateName();
+    validateEmail();
+    validatePassword();
+  }, [data]);
+
+  function validateName() {
+    if (data.name && !REGEX_NAME.test(String(data.name).toLowerCase())) {
+      setNameMessage('Имя может состоять только из русских или латинских букв.');
+      setIsNameValid(false);
+      setIsDisabled(true);
+      if (data.name.length < 2) {
+        return setNameMessage('Минимальная длина: 2 символа.');
+      } else if (data.name.length > 30) {
+        return setNameMessage('Максимальная длина: 30 символов.');
+      }
+    } else {
+      setIsNameValid(true);
+    };
+  }
+
+  function validateEmail() {
+    if (data.email && !REGEX_EMAIL.test(String(data.email).toLowerCase())) {
+      setEmailMessage('E-mail должен содержать "@" и иметь разделяющую точку перед именем домена верхнего уровня.');
+      setIsEmailValid(false);
+      setIsDisabled(true);
+      if (data.email && (data.email.length < 2)) {
+        return setEmailMessage('Минимальная длина: 2 символа.');
+      }
+    } else {
+      setIsEmailValid(true);
+    };
+  };
+
+  function validatePassword() {
+    if (data.password && (data.password.length <= 0)) {
+      setIsPasswordValid(false);
+      setIsDisabled(true);
+    } else {
+      setIsPasswordValid(true);
+    };
+  };
+
+  function validateAll() {
+    if (isNameValid && isEmailValid && isPasswordValid) {
+      if (data.name.length !== 0 && data.email.length !== 0 && data.password.length !== 0) {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      };
+    };
+  };
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+
+    setData({
+      ...data,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleRegistration(data);
+  };
+
+  useEffect(() => {
+    setErrorData('');
+  }, [data]);
+
   return (
     <section className='register'>
       <div></div>
@@ -10,37 +107,73 @@ function Register() {
         <div className='register__logo'></div>
       </Link>
       <h2 className='register__title'>Добро пожаловать!</h2>
-      <form className='register__form'>
+      <form className='register__form' onSubmit={handleSubmit} noValidate>
         <label className='register__label' htmlFor='name'>
           Имя
           <input
-            className='register__input register__input_error'
+            className={`register__input ${!isNameValid && 'register__input_error'}`}
             type='name'
             id='name'
-            value='Я'
+            name='name'
+            autoComplete='on'
+            onChange={handleChange}
             required
           />
-          <span className='register__error'>Ошибка...</span>
+          {
+            !isNameValid && (
+              <span className='register__error'>{nameMessage}</span>
+            )
+          }
         </label>
         <label className='register__label' htmlFor='email'>
           E-mail
           <input
-            className='register__input register__input_active'
+            className={`register__input ${!isEmailValid && 'register__input_error'}`}
             type='email'
             id='email'
-            value='email@mail.ru'
+            name='email'
+            autoComplete='on'
+            onChange={handleChange}
             required />
+            {
+              !isEmailValid && (
+                <span className='register__error'>{emailMessage}</span>
+              )
+            }
         </label>
         <label className='register__label' htmlFor='email'>
           Пароль
           <input
-            className='register__input'
+            className={`register__input ${!isPasswordValid && 'register__input_error'}`}
             type='password'
             id='password'
+            name='password'
+            autoComplete='on'
+            onChange={handleChange}
             required />
+          {
+            !isPasswordValid && (
+              <span className='register__error'>{passwordMessage}</span>
+            )
+          }
         </label>
         <div className='register__auth-container'>
-          <button type='submit' className='register__submit'>Зарегистрироваться</button>
+        {
+            errorData === 400 && (
+              <span className='register__submit-span'>При регистрации пользователя произошла ошибка.</span>
+            )
+          }
+          {
+            errorData === 409 && (
+              <span className='register__submit-span'>Пользователь с таким email уже существует.</span>
+            )
+          }
+          {
+            errorData === 500 && (
+              <span className='register__submit-span'>На сервере произошла ошибка.</span>
+            )
+          }
+          <button type='submit' disabled={isDisabled} className={`register__submit ${isDisabled && 'register__submit_disabled'}`}>Зарегистрироваться</button>
           <p className='register__submit-subtitle'>Уже зарегистрированы? <Link to='/signin' className='register__link'>Войти</Link></p>
         </div>
       </form>
